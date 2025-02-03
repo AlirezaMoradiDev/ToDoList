34a777b4e8163e4c47c9f7a7821ba2dac435bd3c
alireza200411 <alirezamoradi292048@gmail.com>
2025-02-03 23:51:43 +0330

    add app and model task

---
+++ task/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.text import slugify
+
+
+class Task(models.Model):
+    level_priority = [
+        ('low', 'LOW'),
+        ('medium', 'MEDIUM'),
+        ('high', 'HIGH')
+    ]
+
+    status = [
+        ('Not completed', 'NOT COMPLETED'),
+        ('completed', 'COMPLETED')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60, unique=True)
+    description = models.TextField()
+    priority = models.CharField(max_length=15, choices=level_priority)
+    status = models.CharField(max_length=30, choices=status)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save()
+
+    def __str__(self):
+        return self.title
+
