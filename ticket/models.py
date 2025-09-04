d83eda9a1422426052f1cc63addc5b613cf06e88
alireza200411 <alirezamoradi292048@gmail.com>
2025-09-04 19:42:09 +0330

    add: ticket model

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
+from account.models import MyUser
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save()
